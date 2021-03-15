f5e6d5bfff7ea54898d90b5fe7362a5cbfca77aa
Joseph Valbuena <josvalk775@gmail.com>
2021-03-15 11:31:41 -0500

    version 0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HombresComponent } from './components/hombres/hombres.component';
+import { HomeComponent } from './components/home/home.component';
+import { MujeresComponent } from './components/mujeres/mujeres.component';
+import { Page404Component } from './components/page404/page404.component';
+import { PagosComponent } from './components/pagos/pagos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'hombres', component: HombresComponent},
+  {path:'mujeres', component: MujeresComponent},
+  {path:'pagos', component: PagosComponent},
+  {path:'**', pathMatch:'full',component: Page404Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HombresComponent } from './components/hombres/hombres.component';
+import { MujeresComponent } from './components/mujeres/mujeres.component';
+import { PagosComponent } from './components/pagos/pagos.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    HombresComponent,
+    MujeresComponent,
+    PagosComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
